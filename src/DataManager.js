906c96cd250b8d3bfe944f1bcfa31ed20d9b02d6
Kaitlyn Long <katielong11@gmail.com>
2021-09-02 11:46:20 -0500

    created json and js files

---
+++ src/DataManager.js
@@ -0,0 +1,9 @@
+export const getUsers = () => {
+
+    return fetch("http://localhost:8088/users")
+    .then(response => response.json())
+    .then(parsedResponse => {
+        // do something with response here
+        return parsedResponse;
+    })
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+import { getUsers } from "./data/DataManager.js";
+
+ const allUsers = getUsers().then(apiUsers => {
+     console.log("now we can console the users", apiUsers)
+})
+
+console.log("allUsers", allUsers);
+
+
+
